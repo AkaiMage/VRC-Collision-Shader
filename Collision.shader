fd000a35ae25d3de5043f6486f443c42505a320b
AkaiMage <45891066+AkaiMage@users.noreply.github.com>
2019-01-25 22:31:02 -0500

    Added everything.

---
+++ Collision.shader
@@ -0,0 +1,94 @@
+﻿Shader "RedMage/Collision" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Emission ("Emission", 2D) = "black" {}
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+		_CompressionFactor ("Compression Factor", Vector) = (1,1,1,0)
+		_CompressionOffset ("Compression Offset", Vector) = (0,0,0,0)
+		_BlendshapeLookupMap ("Blendshape Lookup Map", 2D) = "black" {}
+		_MapSize ("Map Size", Int) = 16
+		_Interpolator1 ("Interpolator1", Float) = 1
+		_Interpolator2 ("Interpolator2", Float) = 1
+	}
+	SubShader {
+		Tags { "RenderType"="Transparent" "Queue"="Transparent" }
+		LOD 200
+
+		CGPROGRAM
+		#pragma surface surf Standard fullforwardshadows vertex:vert addshadow nofog
+		#pragma exclude_renderers xboxone ps4 n3ds wiiu gles gles3
+		#pragma target 3.0
+		
+		struct Input {
+			float2 uv_MainTex;
+			float2 uv_Emission;
+			
+			float4 screenPos;
+		};
+		struct appdata {
+			float4 vertex : POSITION;
+			float4 tangent : TANGENT;
+			float3 normal : NORMAL;
+			float4 texcoord : TEXCOORD0;
+			float4 texcoord1 : TEXCOORD1;
+			float4 texcoord2 : TEXCOORD2;
+			float4 texcoord3 : TEXCOORD3;
+			fixed4 color : COLOR;
+			uint vid : SV_VertexID;
+			UNITY_VERTEX_INPUT_INSTANCE_ID
+		};
+		
+		sampler2D _BlendshapeLookupMap;
+		int _MapSize;
+		float3 _CompressionFactor, _CompressionOffset;
+
+		sampler2D _MainTex;
+		sampler2D _Emission;
+		half _Glossiness;
+		half _Metallic;
+		fixed4 _Color;
+		
+		float _Interpolator1;
+		float _Interpolator2;
+		
+		sampler2D _CameraDepthTexture;
+		
+		// from StackOverflow
+		inline float DecodeFloatRGBAFixed(float4 enc) {
+			uint ex = (uint) (enc.x * 255);
+			uint ey = (uint) (enc.y * 255);
+			uint ez = (uint) (enc.z * 255);
+			uint ew = (uint) (enc.w * 255);
+			uint v = (ex << 24) + (ey << 16) + (ez << 8) + ew;
+			return ((float) v) / (256.0 * 256.0 * 256.0 * 256.0);
+		}
+
+		void vert (inout appdata v) {
+			float4 xSample = tex2Dlod(_BlendshapeLookupMap, float4(floor(fmod(v.vid * 3, _MapSize)) / (float) _MapSize, floor((v.vid * 3) / (float) _MapSize) / (float) _MapSize, 0, 0));
+			float4 ySample = tex2Dlod(_BlendshapeLookupMap, float4(floor(fmod(v.vid * 3 + 1, _MapSize)) / (float) _MapSize, floor((v.vid * 3 + 1) / (float) _MapSize) / (float) _MapSize, 0, 0));
+			float4 zSample = tex2Dlod(_BlendshapeLookupMap, float4(floor(fmod(v.vid * 3 + 2, _MapSize)) / (float) _MapSize, floor((v.vid * 3 + 2) / (float) _MapSize) / (float) _MapSize, 0, 0));
+			float3 moveTo = 2 * float3(DecodeFloatRGBAFixed(xSample), DecodeFloatRGBAFixed(ySample), DecodeFloatRGBAFixed(zSample)) * _CompressionFactor - _CompressionOffset;
+			
+			float4 clipPos = UnityObjectToClipPos(v.vertex);
+			float4 projPos = ComputeScreenPos(clipPos);
+			projPos.xy /= projPos.w;
+			float sceneZ = LinearEyeDepth(tex2Dlod(_CameraDepthTexture, float4(projPos.xy, 0, 0)).r) * _ProjectionParams.w;
+			float vertexDepth = -UnityObjectToViewPos(v.vertex).z * _ProjectionParams.w;
+			v.vertex.xyz += moveTo * saturate(_Interpolator1 * (1 - _Interpolator2 * smoothstep(0, _ProjectionParams.w, (sceneZ - vertexDepth))));
+		}
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+			o.Emission = tex2D (_Emission, IN.uv_Emission);
+			o.Albedo = c.rgb;
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			o.Alpha = c.a;
+		}
+		
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
---
+++ Editor/EncodeBlendshape.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class EncodeBlendshape : EditorWindow
+{
+	Mesh targetMesh;
+	string blendShapeName;
+
+	[MenuItem("RedMage/Collision Shader/Encode Blendshape")]
+	static void Init() 
+	{
+		EncodeBlendshape window = (EncodeBlendshape) EditorWindow.GetWindow(typeof(EncodeBlendshape));
+		window.Show();
+	}
+	
+	void OnGUI() 
+	{
+		targetMesh = EditorGUILayout.ObjectField("Mesh", targetMesh, typeof(Mesh), true) as Mesh;
+		blendShapeName = EditorGUILayout.TextField("Blendshape Name", blendShapeName);
+		
+		if (GUILayout.Button("Encode Blendshape")) 
+		{
+			int vertexCount = targetMesh.vertexCount;
+			
+			// figure out what texture size to use.
+			// we need 3 values per vertex, (xyz), and we want the texture to be a power of two.
+			// basically, the conditions (imageSize == pow(2,k)) and (imageSize * imageSize >= vertexCount * 3) need to be true
+			// so if you do some math, we'll be rounding sqrt(vertexCount * 3) up to the nearest power of two.
+			// which is what this does.
+			int imageSize = 1 << (FloorLogBase2((int) (Mathf.Sqrt(vertexCount * 3) + .5) - 1) + 1);
+			Texture2D img = new Texture2D(imageSize, imageSize);
+			
+			
+			int shapeIndex = targetMesh.GetBlendShapeIndex(blendShapeName);
+			Vector3[] deltaVertices = new Vector3[vertexCount];
+			targetMesh.GetBlendShapeFrameVertices(shapeIndex, 0, deltaVertices, null, null);
+			
+			// initial scan to determine compression factors
+			Vector3 lowest = new Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity);
+			Vector3 highest = new Vector3(float.NegativeInfinity, float.NegativeInfinity, float.NegativeInfinity);
+			foreach (Vector3 v in deltaVertices)
+			{
+				lowest.x = Mathf.Min(lowest.x, v.x);
+				lowest.y = Mathf.Min(lowest.y, v.y);
+				lowest.z = Mathf.Min(lowest.z, v.z);
+				highest.x = Mathf.Max(highest.x, v.x);
+				highest.y = Mathf.Max(highest.y, v.y);
+				highest.z = Mathf.Max(highest.z, v.z);
+			}
+			
+			// our compression factors to compress/decompress from the [0,1] range of color encoding.
+			Vector3 offset = new Vector3(-lowest.x, -lowest.y, -lowest.z);
+			Vector3 scale = new Vector3(highest.x - lowest.x, highest.y - lowest.y, highest.z - lowest.z);
+			// avoid issues with division by 0
+			if (Mathf.Abs(scale.x) < 1) scale = new Vector3(1, scale.y, scale.z);
+			if (Mathf.Abs(scale.y) < 1) scale = new Vector3(scale.x, 1, scale.z);
+			if (Mathf.Abs(scale.z) < 1) scale = new Vector3(scale.x, scale.y, 1);
+
+			for (int i = 0; i < vertexCount; ++i)
+			{
+				Vector3 v = deltaVertices[i];
+				float x = (v.x + offset.x) / scale.x;
+				float y = (v.y + offset.y) / scale.y;
+				float z = (v.z + offset.z) / scale.z;
+				img.SetPixel((i * 3 + 0) % imageSize, (i * 3 + 0) / imageSize, EncodeFloatRGBA(x * 0.5f));
+				img.SetPixel((i * 3 + 1) % imageSize, (i * 3 + 1) / imageSize, EncodeFloatRGBA(y * 0.5f));
+				img.SetPixel((i * 3 + 2) % imageSize, (i * 3 + 2) / imageSize, EncodeFloatRGBA(z * 0.5f));
+			}
+
+			// Save Assets with needed properties.
+			var saveTo = EditorUtility.SaveFilePanelInProject("Save lookup texture", "", "", "");
+			var imagePath = saveTo + ".png";
+			
+			File.WriteAllBytes(imagePath, img.EncodeToPNG());
+			AssetDatabase.ImportAsset(imagePath);
+			AssetDatabase.SaveAssets();
+			
+			TextureImporter importer = (TextureImporter) TextureImporter.GetAtPath(imagePath);
+			importer.wrapMode = TextureWrapMode.Clamp;
+			importer.mipmapEnabled = false;
+			importer.maxTextureSize = imageSize;
+			importer.sRGBTexture = false;
+			importer.filterMode = FilterMode.Point;
+			importer.textureCompression = TextureImporterCompression.Uncompressed;
+			importer.SaveAndReimport();
+			
+			Material material = new Material(Shader.Find("RedMage/Collision"));
+			material.SetTexture("_BlendshapeLookupMap", (Texture2D) AssetDatabase.LoadAssetAtPath(imagePath, typeof(Texture2D)));
+			material.SetVector("_CompressionFactor", new Vector4(scale.x, scale.y, scale.z, 0));
+			material.SetVector("_CompressionOffset", new Vector4(offset.x, offset.y, offset.z, 0));
+			material.SetInt("_MapSize", imageSize);
+			AssetDatabase.CreateAsset(material, saveTo + ".mat");
+			AssetDatabase.SaveAssets();
+		}
+	}
+	
+	// you can find this on StackOverflow
+	private static Color EncodeFloatRGBA(float v)
+	{
+		var vi = (uint)(v * (256.0f * 256.0f * 256.0f * 256.0f));
+		var ex = (int)(vi / (256 * 256 * 256) % 256);
+		var ey = (int)((vi / (256 * 256)) % 256);
+		var ez = (int)((vi / (256)) % 256);
+		var ew = (int)(vi % 256);
+		return new Color(ex / 255.0f, ey / 255.0f, ez / 255.0f, ew / 255.0f);
+	}
+
+	// you can also find this on StackOverflow
+    private static readonly int[] MultiplyDeBrujinBitPosition = new int[32]
+    {
+        0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
+        8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
+    };
+    private static int FloorLogBase2(int v)
+    {
+        v |= v >> 1; v |= v >> 2; v |= v >> 4; v |= v >> 8; v |= v >> 16;
+        return MultiplyDeBrujinBitPosition[(uint)(v * 0x07C4ACDDU) >> 27];
+    }
+}
